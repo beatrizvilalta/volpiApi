46cb491ddfa0c89fb5511fc05675c88786522efd
Beatriz Vilalta <bia.vilalta@gmail.com>
2024-11-25 13:56:29 -0300

    Create java project

---
+++ src/main/java/com/volpi/api/VolpiApiApplication.java
@@ -0,0 +1,13 @@
+package com.volpi.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolpiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolpiApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/volpi/api/VolpiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volpi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolpiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
